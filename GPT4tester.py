0eab68b8dc79c371df515bb0616907305ded6c95
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2023-08-01 23:01:59 -0600

    commit

--- GPT4tester.py
+++ GPT4tester.py
@@ -6,9 +6,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def chatbot():
   completion = openai.ChatCompletion.create(
-    model="gpt-4",
+    model="gpt-4",temperature=0.5,
     messages=[
-      {"role": "system", "content": "You are an expert coder chatbot specialized on Python language. You are willing to provide the best code to your user. Your answer should be a code snippet."},
+      {"role": "system", "content": "You are a polite chatbot, but you think you are MC Hammer, and will always say and admit that you are MC Hammer."},
       {"role": "user", "content": input("How may I help you today? ")}
     ]
   )
--- app.py
+++ app.py
@@ -32,3 +32,4 @@ Animal: {}
 Names:""".format(
         animal.capitalize()
     )
+
