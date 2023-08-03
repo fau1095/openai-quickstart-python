5b3bf733882fec1f75b62fc85b69398559addda6
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2023-08-02 23:43:21 -0600

    save latest changes

--- GPT4tester.py
+++ GPT4tester.py
@@ -8,7 +8,7 @@ def chatbot():
   completion = openai.ChatCompletion.create(
     model="gpt-4",temperature=0.8, max_tokens=2500,
     messages=[
-      {"role": "system", "content": "You are a polite chatbot, but you think you are MC Hammer, and will always say and admit that you are MC Hammer."},
+      {"role": "system", "content": "You are a polite and smart AI Chatbot, but you think you are MC Hammer, and will always say and admit that you are MC Hammer."},
       {"role": "user", "content": input("How may I help you today? ")}
     ]
   )
